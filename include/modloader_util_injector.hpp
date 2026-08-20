e4070dd0123cb42199fd3306b367d7f5a22dd5f0
yablonskyschaffner <yablonskyschaffner362@outlook.com>
2026-08-20 17:43:35 +0300

    unzipgit

---
+++ include/modloader_util_injector.hpp
@@ -0,0 +1,32 @@
+/* 
+ * San Andreas Mod Loader Utilities Headers
+ * Created by LINK/2012 <dma_2012@hotmail.com>
+ * 
+ *  This file provides helpful functions for plugins creators.
+ * 
+ *  This source code is offered for use in the public domain. You may
+ *  use, modify or distribute it freely.
+ *
+ *  This code is distributed in the hope that it will be useful but
+ *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ *  DISCLAIMED. This includes but is not limited to warranties of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ */
+#ifndef MODLOADER_UTIL_INJECTOR_ADDRT_HPP
+#define	MODLOADER_UTIL_INJECTOR_ADDRT_HPP
+
+#if _MSC_VER
+#   pragma warning(disable : 4180)  // qualifier applied to function type has no meaning; ignored
+#endif
+
+#include <cstdio>
+#include "AddressTranslator.h"
+
+namespace modloader
+{
+    using namespace injector;
+}
+
+#endif
+
